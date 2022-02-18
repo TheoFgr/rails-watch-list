3a426c6ec143170dbdd005e6bb5e974c3bd9758e
theo faugere <theo.faugere3@gmail.com>
2022-02-18 11:51:39 +0100

    try to fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :lists, except: [:destroy, :edit, :update] do
+    resources :bookmarks, only: [:new, :create, :destroy]
+  end
 end
