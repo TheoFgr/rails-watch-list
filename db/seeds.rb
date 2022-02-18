81bc6f24b290cf4ed914adfa0abc29c090ece226
theo faugere <theo.faugere3@gmail.com>
2022-02-18 15:15:16 +0100

    creation de movies

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,8 @@
+
+Movie.destroy_all
+
+puts "all the movies are destroy"
+
 Movie.create(title: "Wonder Woman 1984", overview: "Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s", poster_url: "https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg", rating: 6.9)
 Movie.create(title: "The Shawshank Redemption", overview: "Framed in the 1940s for double murder, upstanding banker Andy Dufresne begins a new life at the Shawshank prison", poster_url: "https://image.tmdb.org/t/p/original/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg", rating: 8.7)
 Movie.create(title: "Titanic", overview: "101-year-old Rose DeWitt Bukater tells the story of her life aboard the Titanic.", poster_url: "https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg", rating: 7.9)
@@ -6,3 +11,5 @@ Movie.create(title: "Ocean's Eight", overview: "Debbie Ocean, a criminal masterm
 10.times do
   movie = Movie.create(title: Faker::Movie.title, overview: Faker::Movie.quote, rating: rand(1..10))
 end
+
+puts 'movies created'
